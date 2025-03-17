e3b4789314ee3c752cc861788acd3b0be4d6ba63
ivan_novik <nvikivan280@gmail.com>
2025-03-17 20:37:56 +0000

    Added all the conditions for Shape.java with the other shapes classes

--- src/main/java/griffith/Shape.java
+++ src/main/java/griffith/Shape.java
@@ -1,5 +1,136 @@
 package griffith;
 
-public class Shape {
+abstract class Shape {
+    private String name;
 
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract double area();
+    public abstract double perimeter();
+
+    @Override
+    public String toString() {
+        return "Shape: " + name;
+    }
 }
+
+// Circle subclass
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(String name, double radius) {
+        super(name);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Radius: " + radius;
+    }
+}
+
+// Rhombus subclass
+class Rhombus extends Shape {
+    private double diagonal1;
+    private double diagonal2;
+    private double side;
+
+    public Rhombus(String name, double diagonal1, double diagonal2, double side) {
+        super(name);
+        this.diagonal1 = diagonal1;
+        this.diagonal2 = diagonal2;
+        this.side = side;
+    }
+
+    public double getDiagonal1() {
+        return diagonal1;
+    }
+
+    public double getDiagonal2() {
+        return diagonal2;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    @Override
+    public double area() {
+        return (diagonal1 * diagonal2) / 2;
+    }
+
+    @Override
+    public double perimeter() {
+        return 4 * side;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Diagonals: " + diagonal1 + " & " + diagonal2 + ", Side: " + side;
+    }
+}
+
+// Right-angled Triangle subclass
+class RightAngledTriangle extends Shape {
+    private double base;
+    private double height;
+
+    public RightAngledTriangle(String name, double base, double height) {
+        super(name);
+        this.base = base;
+        this.height = height;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public double area() {
+        return 0.5 * base * height;
+    }
+
+    @Override
+    public double perimeter() {
+        double hypotenuse = Math.sqrt((base * base) + (height * height));
+        return base + height + hypotenuse;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Base: " + base + ", Height: " + height;
+    }
+}
+
